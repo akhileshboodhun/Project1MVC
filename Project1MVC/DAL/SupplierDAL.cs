95366985d5c6859291516f7fe7f0c1c14d51e742
Akhilesh Boodhun <quantum19992011@gmail.com>
2021-10-19 13:50:56 +0400

    DAL folder

---
+++ Project1MVC/DAL/SupplierDAL.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Project1MVC.DAL
+{
+    public class SupplierDAL
+    {
+        private SqlConnection con;
+        private void connection()
+        {
+            string constring = ConfigurationManager.ConnectionStrings["ItStockDBConnection"].ToString();
+            con = new SqlConnection(constring);
+        }
+
+        // **************** ADD NEW SUPPLIER *********************
+        public bool AddStudent(Supplier supplier)
+        {
+            connection();
+            SqlCommand cmd = new SqlCommand("AddNewSupplier", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@Name", supplier.Name);
+            cmd.Parameters.AddWithValue("@City", supplier.City);
+            cmd.Parameters.AddWithValue("@Address", supplier.Address);
+
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+
+            if (i >= 1)
+                return true;
+            else
+                return false;
+        }
+    }
+}
