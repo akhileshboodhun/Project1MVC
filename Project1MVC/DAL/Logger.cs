9d5309ba1e2959ce050be273b540bc50ae5f5c5b
Muzaffar Auhammud <Muzaffar.Auhammud@ceridian.com>
2021-10-25 19:45:18 +0400

    Add Logger.cs

---
+++ Project1MVC/DAL/Logger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace Project1MVC.DAL
+{
+    public static class Logger
+    {
+        public static void Log(string message)
+        {
+            Debug.WriteLine(message);
+        }
+    }
+}
