58f336e2f630bd3d052c077fbaf5362fc68cb779
Muzaffar Auhammud <Muzaffar.Auhammud@ceridian.com>
2021-10-25 22:34:10 +0400

    Implement DAL to manage connection to the database

---
+++ Project1MVC/DAL/DAL.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data.SqlClient;
+using Project1MVC.Services;
+
+namespace Project1MVC.DAL
+{
+    public static class DAL
+    {
+        private static SqlConnection conn = null;
+
+        public static SqlConnection GetConnection()
+        {
+            if (conn != null)
+            {
+                Logger.Log("Reusing current SqlConnection");
+                return conn;
+            }
+            else
+            {
+                string dbServerName = @"localhost\SQLEXPRESS";
+                string dbName = "ITStock";
+                //string dbUsername = "";
+                //string dbPassword = "";
+
+                string connString = $"Data Source = {dbServerName}; Initial Catalog = {dbName}; Integrated Security = True";
+
+                try
+                {
+                    Logger.Log("Creating new SqlConnection");
+                    conn = new SqlConnection(connString);
+                    conn.InfoMessage += Conn_InfoMessage;
+
+                    Logger.Log("Opening the SqlConnection");
+                    conn.Open();
+                    return conn;
+                }
+                catch (Exception ex)
+                {
+                    Logger.Log(ex.Message);
+                    return null;
+                }
+            }
+        }
+
+        private static void Conn_InfoMessage(object sender, SqlInfoMessageEventArgs e)
+        {
+            Logger.Log(e.Message);
+        }
+    }
+}
