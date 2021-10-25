29e99359fb9a626507320ea63c1c03d8b3c292f1
Muzaffar Auhammud <Muzaffar.Auhammud@ceridian.com>
2021-10-25 22:32:01 +0400

    Improve Logger to include DateTime in logs and write the log to a text file

--- Project1MVC/Services/Logger.cs
+++ Project1MVC/Services/Logger.cs
@@ -11,6 +11,11 @@ namespace Project1MVC.Services
         public static void Log(string message)
         {
             Debug.WriteLine(message);
+
+            string logfilePath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "bin");
+            string text = $"[{DateTime.Now.ToString()}]: {message + Environment.NewLine}";
+
+            System.IO.File.AppendAllText(logfilePath + @"\log.txt", text);
         }
     }
 }
