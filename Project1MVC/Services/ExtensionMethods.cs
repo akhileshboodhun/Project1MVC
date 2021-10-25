3f63a7da064730d7c4e6baccf481e61446ba8a8d
Muzaffar Auhammud <Muzaffar.Auhammud@ceridian.com>
2021-10-25 23:21:13 +0400

    Implement ExtensionMethods to convert string and object to int

---
+++ Project1MVC/Services/ExtensionMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project1MVC.Services
+{
+    public static class ExtensionMethods
+    {
+        public static int ToInt(this string text)
+        {
+            return int.Parse(text);
+        }
+
+        public static int ToInt(this object obj)
+        {
+            return Convert.ToInt32(obj);
+        }
+    }
+}
