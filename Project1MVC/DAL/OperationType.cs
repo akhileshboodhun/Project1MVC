5a777c97eda7b8862135f2eb9897dacae0dc0e84
Muzaffar Auhammud <Muzaffar.Auhammud@ceridian.com>
2021-10-26 20:55:52 +0400

    Add enum OperationType

---
+++ Project1MVC/DAL/OperationType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project1MVC.DAL
+{
+    public enum OperationType
+    {
+        Get,
+        GetAll,
+        Add,
+        Update,
+        Delete,
+    }
+}
