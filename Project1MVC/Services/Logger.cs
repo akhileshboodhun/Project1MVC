3a9f43bf7d4ab15607b5c5950926889d8f8f1e13
Muzaffar Auhammud <Muzaffar.Auhammud@ceridian.com>
2021-10-27 12:53:38 +0400

    Remove Log() implementation for Logger

--- Project1MVC/Services/Logger.cs
+++ Project1MVC/Services/Logger.cs
@@ -6,21 +6,21 @@ using System.Web;
 using System.Threading;
 using System.IO;
 using System.Threading.Tasks;
+using System.Text;
 
 namespace Project1MVC.Services
 {
     public static class Logger
     {
+        //static string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "bin");
+        //static string filename = path + @"\log.txt";
+
         public static void Log(string message)
         {
-            Debug.WriteLine(message);
+            //Debug.WriteLine(message);
 
-            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "bin");
-            string filename = path + @"\log.txt";
-
-            string text = $"[{DateTime.Now.ToString()}]: {message + Environment.NewLine}";
-
-            File.AppendAllText(filename, text);
+            //string text = $"[{DateTime.Now.ToString()}]: {message + Environment.NewLine}";
+            //Task.Run(() => File.AppendAllText(filename, text));
         }
     }
 }
