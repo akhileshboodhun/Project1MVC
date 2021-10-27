4886c62c907dff5a7abca09ef50eb0e724a5e6d1
Muzaffar Auhammud <Muzaffar.Auhammud@ceridian.com>
2021-10-27 12:53:49 +0400

    Fix empty table issue in DAL

--- Project1MVC/DAL/DAL.cs
+++ Project1MVC/DAL/DAL.cs
@@ -6,45 +6,50 @@ namespace Project1MVC.DAL
 {
     public static class DAL
     {
-        private static SqlConnection conn = null;
-
         public static SqlConnection GetConnection()
         {
-            if (conn != null)
+            string dbServerName = @"localhost\SQLEXPRESS";
+            string dbName = "ITStock";
+            //string dbUsername = "";
+            //string dbPassword = "";
+
+            string connString = $"Data Source = {dbServerName}; Initial Catalog = {dbName}; Integrated Security = True";
+
+            try
             {
-                Logger.Log("Reusing current SqlConnection");
+                SqlConnection conn = new SqlConnection(connString);
+                //conn.InfoMessage += Conn_InfoMessage;
+                //conn.StateChange += Conn_StateChange;
+                Logger.Log("SqlConnection created");
+
+                conn.Open();
                 return conn;
             }
-            else
+            catch (Exception ex)
             {
-                string dbServerName = @"localhost\SQLEXPRESS";
-                string dbName = "ITStock";
-                //string dbUsername = "";
-                //string dbPassword = "";
-
-                string connString = $"Data Source = {dbServerName}; Initial Catalog = {dbName}; Integrated Security = True";
-
-                try
-                {
-                    Logger.Log("Creating new SqlConnection");
-                    conn = new SqlConnection(connString);
-                    conn.InfoMessage += Conn_InfoMessage;
-
-                    Logger.Log("Opening the SqlConnection");
-                    conn.Open();
-                    return conn;
-                }
-                catch (Exception ex)
-                {
-                    Logger.Log(ex.Message);
-                    return null;
-                }
+                Logger.Log(ex.Message);
+                //Logger.Flush();
+                return null;
             }
+        }
+
+        private static void Conn_StateChange(object sender, System.Data.StateChangeEventArgs e)
+        {
+            //if (e.CurrentState.ToString().Contains("close"))
+            //{
+            //    Logger.Log("SqlConnection closed" + Environment.NewLine);
+            //}
+            //else if (e.CurrentState.ToString().Contains("open"))
+            //{
+            //    Logger.Log("SqlConnection opened");
+            //}
+
+            //Logger.Flush();
         }
 
         private static void Conn_InfoMessage(object sender, SqlInfoMessageEventArgs e)
         {
-            Logger.Log(e.Message);
+            //Logger.Log(e.Message);
         }
     }
 }
