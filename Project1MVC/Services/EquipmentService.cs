0df392b05be163be66761f064fee7972ae3f571c
Muzaffar Auhammud <Muzaffar.Auhammud@ceridian.com>
2021-11-03 10:16:43 +0400

    Add EquipmentService

---
+++ Project1MVC/Services/EquipmentService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Project1MVC.DAL;
+using Project1MVC.Models;
+
+namespace Project1MVC.Services
+{
+    public class EquipmentService : IEquipmentService
+    {
+        private IEquipmentRepository equipmentRepository;
+
+        public EquipmentService(IEquipmentRepository repository)
+        {
+            equipmentRepository = repository;
+        }
+
+        public bool Add(Equipment obj)
+        {
+            return equipmentRepository.Add(obj);
+        }
+
+        public void ReturnFromEmployee(int equipId, int empId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AssignToEmployee(int equipId, int empId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            return equipmentRepository.Delete(id);
+        }
+
+        public Equipment Get(int id)
+        {
+            return equipmentRepository.Get(id);
+        }
+
+        public IList<Equipment> GetAll()
+        {
+            return equipmentRepository.GetAll();
+        }
+
+        public bool Update(Equipment obj)
+        {
+            return equipmentRepository.Update(obj);
+        }
+    }
+}
---
+++ Project1MVC/Services/IEquipmentService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Project1MVC.DAL;
+
+namespace Project1MVC.Services
+{
+    public interface IEquipmentService : IEquipmentRepository
+    {
+        bool AssignToEmployee(int equipId, int empId);
+
+        void ReturnFromEmployee(int equipId, int empId);
+    }
+}
