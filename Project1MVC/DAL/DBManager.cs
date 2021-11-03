15dd87f896867a53841aa294faaf8498a49de3f4
Muzaffar Auhammud <Muzaffar.Auhammud@ceridian.com>
2021-11-03 10:15:06 +0400

    Refactor DBManager and IDBProvider

--- Project1MVC/DAL/DBManager.cs
+++ Project1MVC/DAL/DBManager.cs
@@ -4,23 +4,50 @@ using Project1MVC.Services;
 
 namespace Project1MVC.DAL
 {
-    public static class DBManager
+    public class DBManager : IDBProvider
     {
-        public static SqlConnection GetConnection()
+        private DBManager() { }
+
+        public static DBManager Instance { get { return Nested.instance; } }
+
+        private class Nested
         {
-            string dbServerName = @"localhost\SQLEXPRESS";
-            string dbName = "ITStock";
-            //string dbUsername = "";
-            //string dbPassword = "";
+            // Explicit static constructor to tell C# compiler not to lazily instantiate us
+            static Nested() { }
 
-            string connString = $"Data Source = {dbServerName}; Initial Catalog = {dbName}; Integrated Security = True";
+            internal static readonly DBManager instance = new DBManager();
+        }
 
+        public string ServerName
+        {
+            get
+            {
+                return @"localhost\SQLEXPRESS";
+            }
+        }
+
+        public string DatabaseName
+        {
+            get
+            {
+                return "ITStock";
+            }
+        }
+
+        public string ConnectionString
+        {
+            get
+            {
+                return $"Data Source = {this.ServerName}; Initial Catalog = {this.DatabaseName}; Integrated Security = True";
+            }
+        }
+
+        public SqlConnection GetConnection()
+        {
             try
             {
-                SqlConnection conn = new SqlConnection(connString);
+                SqlConnection conn = new SqlConnection(this.ConnectionString);
                 //conn.InfoMessage += Conn_InfoMessage;
-                //conn.StateChange += Conn_StateChange;
-                Logger.Log("SqlConnection created");
 
                 conn.Open();
                 return conn;
@@ -28,23 +55,8 @@ namespace Project1MVC.DAL
             catch (Exception ex)
             {
                 Logger.Log(ex.Message);
-                //Logger.Flush();
                 return null;
             }
-        }
-
-        private static void Conn_StateChange(object sender, System.Data.StateChangeEventArgs e)
-        {
-            //if (e.CurrentState.ToString().Contains("close"))
-            //{
-            //    Logger.Log("SqlConnection closed" + Environment.NewLine);
-            //}
-            //else if (e.CurrentState.ToString().Contains("open"))
-            //{
-            //    Logger.Log("SqlConnection opened");
-            //}
-
-            //Logger.Flush();
         }
 
         private static void Conn_InfoMessage(object sender, SqlInfoMessageEventArgs e)
