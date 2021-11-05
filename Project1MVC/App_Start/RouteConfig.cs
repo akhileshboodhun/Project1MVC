6e632019981de80e685e05d34e8b7f807dd865b9
Dharmesh Sookun <Dharmesh.Sookun@ceridian.com>
2021-11-05 08:39:18 +0400

    #80 Order CRUD

--- Project1MVC/App_Start/RouteConfig.cs
+++ Project1MVC/App_Start/RouteConfig.cs
@@ -13,6 +13,12 @@ namespace Project1MVC
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //routes.MapRoute(
+            //    name: "OrderUpdate",
+            //    url: "{controller}/{action}/{orderId}/",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ Project1MVC/Models/Order.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Project1MVC.Models
+{
+    public class Order
+    {
+        public Order() { }
+
+        public Order(bool isOrderComplete, DateTime orderDate, int supplierId)
+        {
+            this.IsOrderComplete = isOrderComplete;
+            this.OrderDate = orderDate;
+            this.SupplierId = supplierId;
+        }
+
+        public Order(int? id, bool isOrderComplete, DateTime orderDate, int supplierId) : this(isOrderComplete, orderDate, supplierId)
+        {
+            this.Id = id;
+        }
+
+        public Order(int? id, bool isOrderComplete, DateTime orderDate, string supplierName)
+        {
+            this.Id = id;
+            this.IsOrderComplete = isOrderComplete;
+            this.OrderDate = orderDate;
+            this.SupplierName = supplierName;
+        }
+
+        public int? Id { get; set; }
+
+        [Display(Name = "Order Completed")]
+        public bool IsOrderComplete { get; set; }
+
+        [Display(Name = "Order Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime OrderDate { get; set; }
+        public int SupplierId { get; set; }
+
+        [Display(Name = "Supplier Name")]
+        public string SupplierName { get; set; }
+    }
+}
