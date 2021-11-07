b0832b64040a85d3a33ed502d46befeefd5d2d8d
Muzaffar Auhammud <muz.ahd@pm.me>
2021-11-07 21:15:49 +0400

    Add IDBProvider.cs

---
+++ Project1MVC/DAL/IDBProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Project1MVC.DAL
+{
+    public interface IDBProvider
+    {
+        string ServerName { get; }
+
+        string DatabaseName { get; }
+
+        string ConnectionString { get; }
+
+        SqlConnection GetConnection();
+    }
+}
