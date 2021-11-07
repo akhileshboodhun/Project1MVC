9519b2cd4c40a267ebac4a8562815d680b4109be
Muzaffar Auhammud <muz.ahd@pm.me>
2021-11-07 21:48:13 +0400

    Integrate EquipmentService and EquipmentRepository with Unity

--- Project1MVC/App_Start/UnityConfig.cs
+++ Project1MVC/App_Start/UnityConfig.cs
@@ -1,5 +1,9 @@
+using Project1MVC.DAL;
+using Project1MVC.Models;
+using Project1MVC.Services;
 using System.Web.Mvc;
 using Unity;
+using Unity.Lifetime;
 using Unity.Mvc5;
 
 namespace Project1MVC
@@ -9,12 +13,13 @@ namespace Project1MVC
         public static void RegisterComponents()
         {
 			var container = new UnityContainer();
-            
+
             // register all your components with the container here
             // it is NOT necessary to register your controllers
-            
-            // e.g. container.RegisterType<ITestService, TestService>();
-            
+
+            container.RegisterType<IEquipmentService, EquipmentService>(new PerThreadLifetimeManager());
+            container.RegisterInstance<IRepository<Equipment>>(EquipmentRepository.Instance);
+
             DependencyResolver.SetResolver(new UnityDependencyResolver(container));
         }
     }
