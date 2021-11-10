b53ff550bbe3cb74f9029217577ed8b6fb32757f
Muzaffar Auhammud <muz.ahd@pm.me>
2021-11-10 19:20:03 +0400

    Finalize IEquipmentService.cs

--- Project1MVC/Services/IEquipmentService.cs
+++ Project1MVC/Services/IEquipmentService.cs
@@ -1,13 +1,24 @@
 ﻿using System.Collections.Generic;
-using Project1MVC.DAL;
 using Project1MVC.Models;
 
 namespace Project1MVC.Services
 {
-    public interface IEquipmentService : IRepository<Equipment>
+    public interface IEquipmentService
     {
         bool AssignToEmployee(int equipId, int empId, int assignorId);
 
         bool TakeFromEmployee(int equipId, int empId);
+
+        bool Add(Equipment obj);
+
+        Equipment Get(int id);
+
+        Equipment Get(int id, IList<string> cols);
+
+        IList<Equipment> GetAll();
+
+        IList<Equipment> GetPaginatedList(int? pageNumber, int? pageSize, IList<string> cols = null, string sortBy = "", string sortOrder = "");
+
+        bool Update(Equipment obj);
     }
 }
