678c47e225e517e6beef2635478478e66ba2941b
Muzaffar Auhammud <muz.ahd@pm.me>
2021-11-13 01:38:05 +0400

    Improve constructor of Filter.cs

--- Project1MVC/Services/Filter.cs
+++ Project1MVC/Services/Filter.cs
@@ -7,32 +7,12 @@ namespace Project1MVC.Services
 {
     public class Filter
     {
-        public Filter(string columnName, object searchValue1, object searchValue2, FilterType type)
+        public Filter(string columnName, string searchValue1, string searchValue2, FilterType type)
         {
             ColumnName = columnName;
+            FilterType = type;
             SearchValue1 = searchValue1;
             SearchValue2 = searchValue2;
-            FilterType = type;
-
-            if (type == FilterType.Range)
-            {
-                if (searchValue2 == null && searchValue1 != null)
-                {
-                    SearchValue1 = searchValue1;
-                    SearchValue2 = int.MaxValue;
-                }
-                else if (searchValue1 == null && searchValue2 != null)
-                {
-                    SearchValue1 = int.MinValue;
-                    SearchValue2 = searchValue2;
-                }
-                else if (searchValue1 == null && searchValue2 == null)
-                {
-                    throw new InvalidOperationException("Both searchValue1 and searchValue2 cannot be null when FilterType=Range is used.");
-                }
-            }
-
-            
         }
 
         public string ColumnName
@@ -40,12 +20,12 @@ namespace Project1MVC.Services
             get; private set;
         }
 
-        public object SearchValue1
+        public string SearchValue1
         {
             get; private set;
         }
 
-        public object SearchValue2
+        public string SearchValue2
         {
             get; private set;
         }
