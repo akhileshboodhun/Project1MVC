f51e106904af32ec55d6a30845007585049fb606
Muzaffar Auhammud <muz.ahd@pm.me>
2021-11-16 23:59:42 +0400

    Implement PaginatedListInfo.cs

--- Project1MVC/Services/PaginatedListInfo.cs
+++ Project1MVC/Services/PaginatedListInfo.cs
@@ -1,20 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 
 namespace Project1MVC.Services
 {
     public class PaginatedListInfo<T>
     {
-        public PaginatedListInfo(int pageNumber, int pageSize, int pageCount, int recordsCount, string sortBy, string sortOrder)
+        public PaginatedListInfo(string pageNumber, string pageSize, int recordsCount, string sortBy, string sortOrder)
         {
-            this.PageNumber = pageNumber;
-            this.PageSize = pageSize;
+            int _pageNumber = ServicesHelper.SanitizePageNumber(pageNumber);
+            int _pageSize = ServicesHelper.SanitizePageSize(pageSize);
+            string _sortBy = ServicesHelper.SanitizeSortBy<T>(sortBy);
+            string _sortOrder = ServicesHelper.SanitizeSortOrder(sortOrder);
+
+            int remainder;
+            int pageCount = Math.DivRem(recordsCount, _pageSize, out remainder);
+            pageCount = (remainder == 0) ? pageCount : pageCount + 1;
+            pageCount = pageCount < 1 ? 1 : pageCount;
+
+            int adjustedPageNumber =
+                ((recordsCount - (_pageNumber * _pageSize)) < 0) && (_pageNumber != pageCount) ?
+                1 : _pageNumber;
+
+            this.PageNumber = adjustedPageNumber;
+            this.PageSize = _pageSize;
             this.PageCount = pageCount;
             this.RecordsCount = recordsCount;
-            this.SortBy = sortBy;
-            this.SortOrder = sortOrder;
+            this.SortBy = _sortBy;
+            this.SortOrder = _sortOrder;
             this.DisplayPrimaryColumn = false;
             this.DisplayColumns = new List<string>();
         }
