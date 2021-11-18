b5aefd1f0c0fbde9caf2c22982666c74eee2a897
Muzaffar Auhammud <muz.ahd@pm.me>
2021-11-18 12:49:59 +0400

    Add unit tests

---
+++ Project1MVC/Tests/Tests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using Project1MVC.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project1MVC.Tests
+{
+    public class Tests
+    {
+        [TestCase("de")]
+        [TestCase("desc")]
+        [TestCase("descending")]
+        [TestCase("degffgbgf")]
+        public void SanitizeSortOrderForDescending(string sortOrder)
+        {
+            string result = ServicesHelper.SanitizeSortOrder(sortOrder);
+            Assert.That(result, Is.EqualTo("desc"));
+        }
+
+        [TestCase("asc")]
+        [TestCase("ascend")]
+        [TestCase("degffgbgf")]
+        [TestCase(" ")]
+        [TestCase("")]
+        public void SanitizeSortOrderForAscending(string sortOrder)
+        {
+            var result = ServicesHelper.SanitizeSortOrder(sortOrder);
+            Assert.That(result, Is.EqualTo("asc"));
+        }
+    }
+}
