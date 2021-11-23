415bd5af918ac542b42dbfd6811430ffbb851439
Dharmesh Sookun <Dharmesh.Sookun@ceridian.com>
2021-11-23 09:51:51 +0400

    Added Required attribute to properties in Supplier Model and removed delete links from Views

--- Project1MVC/Models/Supplier.cs
+++ Project1MVC/Models/Supplier.cs
@@ -26,10 +26,14 @@ namespace Project1MVC.Models
 
         public int? SupplierId { get; set; }
 
+        [Required]
         [MaxLength(100), MinLength(3)]
         public string Name { get; set; }
+        [Required]
         [MaxLength(15), MinLength(3)]
+        [RegularExpression(@"[0-9]{3,15}", ErrorMessage = "Invalid phone number")]
         public string PhoneNo { get; set; }
+        [Required]
         [MaxLength(250), MinLength(3)]
         public string Address { get; set; }
     }
